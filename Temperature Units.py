b7431874779968d7d6bf4e9de16945fd320a0eb9
AkamakaKio <147234894+AkamakaKio@users.noreply.github.com>
2024-02-27 17:29:41 +0100

    Add files via upload

---
+++ Temperature Units.py
@@ -0,0 +1,7 @@
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9/5 + 32
+
+# Example usage:
+celsius_temp = 30
+fahrenheit_temp = celsius_to_fahrenheit(celsius_temp)
+print("Fahrenheit temperature:", fahrenheit_temp)
